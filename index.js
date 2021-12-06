670b2b7d9a0d3ea68dae791879f6b8f2079c7a40
Yash Khare <60147732+khareyash05@users.noreply.github.com>
2021-12-06 22:31:34 +0530

    connected login page to mongodb database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const { Auth } = require("two-step-auth");
 const bodyParser = require('body-parser')
+const dotenv= require("dotenv")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 const alert = require('alert')
 
 const app = express()
@@ -9,6 +12,11 @@ app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+dotenv.config({path : 'config.env'})
+
+require("./db/conn")
+const User = require("./model/userSchema")
 
 var check ;
 var mail;
@@ -56,7 +64,50 @@ app.get("/details",(req,res)=>{
     res.render("details")
 })
 
+app.post("/details",async(req,res)=>{
+    const {name,email,phone,password,cpassword} = req.body
+    
+    if(!name||!email||!phone||!password||!cpassword){
+        alert("FIll the field")
+    }   
+    
+    try{
+        const userExist = await User.findOne({email : email})
+        if(userExist){
+            alert("User Already Exist")
+            res.redirect("/details")
+        }
+        else if(password !== cpassword){
+            alert("Password Not Matched")
+            res.redirect("/details")
+        }
+        else{
+            bcrypt.hash(password,10,function(err,hash){
+                const newUser = new User({
+                    name : name,
+                    email : email,
+                    phone : phone,                    
+                    password : hash,
+                    cpassword:hash
+                })
+                newUser.save((err)=>{
+                    if(err){
+                        console.log(err);
+                    }
+                    else { 
+                        alert("User Created")
+                        res.redirect("/")
+                    }
+                })       
+            })
+        }
+                
+    }catch(err){
+        console.log(err);
+    }    
+})
+
 const port = process.env.PORT || 3000
 app.listen(port, () => {
-    console.log("Server running on port 3000")
+    console.log("Server running on port " + port)
 })
