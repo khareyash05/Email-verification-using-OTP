1755f16e1cc9f74b306d173bf7f4d59286d9fa5e
Yash Khare <60147732+khareyash05@users.noreply.github.com>
2021-12-11 08:44:46 +0530

    minor box shadow changes in css

--- db/conn.js
+++ db/conn.js
@@ -5,8 +5,6 @@ const DB = process.env.DATABASE
 mongoose.connect(DB,{
     useNewUrlParser : true,
     useUnifiedTopology : true,
-    useCreateIndex : true,
-    useFindAndModify : false
 }).then(() =>{
     console.log(`Successfull connection`)
 }).catch((err) => console.log(`no connection`))
